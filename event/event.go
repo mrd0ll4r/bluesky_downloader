406b4bca671b340f13c6fb96f38206dbe8bbeae9
Leo Balduf <leobalduf@gmail.com>
2025-02-27 14:30:15 +0100

    event: add deprecation notes on unused event types

--- event/event.go
+++ event/event.go
@@ -14,14 +14,19 @@ type JsonErrorFrame struct {
 
 // A JsonEvent is an event logged from any of the ATProto event streams, such
 // as the Firehose or a Labeler's stream of labels.
+//
+// TODO add RepoSync once it's rolled out.
 type JsonEvent struct {
-	Received      time.Time                             `json:"received"`
-	RepoCommit    *atproto.SyncSubscribeRepos_Commit    `json:"repo_commit,omitempty"`
-	RepoHandle    *atproto.SyncSubscribeRepos_Handle    `json:"repo_handle,omitempty"`
-	RepoIdentity  *atproto.SyncSubscribeRepos_Identity  `json:"repo_identity,omitempty"`
-	RepoAccount   *atproto.SyncSubscribeRepos_Account   `json:"repo_account,omitempty"`
-	RepoInfo      *atproto.SyncSubscribeRepos_Info      `json:"repo_info,omitempty"`
-	RepoMigrate   *atproto.SyncSubscribeRepos_Migrate   `json:"repo_migrate,omitempty"`
+	Received   time.Time                          `json:"received"`
+	RepoCommit *atproto.SyncSubscribeRepos_Commit `json:"repo_commit,omitempty"`
+	// DEPRECATED
+	RepoHandle   *atproto.SyncSubscribeRepos_Handle   `json:"repo_handle,omitempty"`
+	RepoIdentity *atproto.SyncSubscribeRepos_Identity `json:"repo_identity,omitempty"`
+	RepoAccount  *atproto.SyncSubscribeRepos_Account  `json:"repo_account,omitempty"`
+	RepoInfo     *atproto.SyncSubscribeRepos_Info     `json:"repo_info,omitempty"`
+	// DEPRECATED
+	RepoMigrate *atproto.SyncSubscribeRepos_Migrate `json:"repo_migrate,omitempty"`
+	// DEPRECATED
 	RepoTombstone *atproto.SyncSubscribeRepos_Tombstone `json:"repo_tombstone,omitempty"`
 	LabelLabels   *atproto.LabelSubscribeLabels_Labels  `json:"label_labels,omitempty"`
 	LabelInfo     *atproto.LabelSubscribeLabels_Info    `json:"label_info,omitempty"`
